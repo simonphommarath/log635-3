13b76235023af8c49a101b8aaff66267e670f702
Yannick Maringo <yannickmaringo123@gmail.com>
2017-03-26 14:55:19 -0400

    Added IMPORTANCE level on each data (Selected 10 for now, will have to discuss about it in team), Added Normalization (working for APM At The Moment)

--- main.cpp
+++ main.cpp
@@ -15,136 +15,136 @@
 
 class Player
 {
-  public :
-    int GameID;
-    int LeagueIndex;
-    int ActualLeagueIndex;
-    int Age;
-    int HoursPerWeek;
-    int TotalHours;
-    float APM;
-    float SelectByHotkeys;
-    float AssignToHotkeys;
-    float UniqueHotkeys;
-    float MinimapAttacks;
-    float MinimapRightClicks;
-    float NumberOfPACs;
-    float GapBetweenPACs;
-    float ActionLatency;
-    float ActionsInPAC;
-    float TotalMapExplored;
-    float WorkersMade;
-    float UniqueUnitsMade;
-    float ComplexUnitsMade;
-    float ComplexAbilitiesUsed;
+public:
+	int GameID;
+	int LeagueIndex;
+	int ActualLeagueIndex;
+	int Age;
+	int HoursPerWeek;
+	int TotalHours;
+	float APM;
+	float SelectByHotkeys;
+	float AssignToHotkeys;
+	float UniqueHotkeys;
+	float MinimapAttacks;
+	float MinimapRightClicks;
+	float NumberOfPACs;
+	float GapBetweenPACs;
+	float ActionLatency;
+	float ActionsInPAC;
+	float TotalMapExplored;
+	float WorkersMade;
+	float UniqueUnitsMade;
+	float ComplexUnitsMade;
+	float ComplexAbilitiesUsed;
 
 };
 
 
 std::vector<std::string> split(const std::string &s, char c) {
-    std::string buff{""};
-    std::vector<std::string> v;
+	std::string buff{ "" };
+	std::vector<std::string> v;
 
-        for(auto n:s)
-        {
-            if(n != c) buff+=n;
-            else if(n == c && buff != "")
-            {
-                v.push_back(buff);
-                buff = "";
-            }
-        }
-        if(buff != "")
-            v.push_back(buff);
+	for (auto n : s)
+	{
+		if (n != c) buff += n;
+		else if (n == c && buff != "")
+		{
+			v.push_back(buff);
+			buff = "";
+		}
+	}
+	if (buff != "")
+		v.push_back(buff);
 
-        return v;
+	return v;
 }
 
-std::string remove_char( std::string str, char ch )
+std::string remove_char(std::string str, char ch)
 {
-    // remove all occurrences of char ch from str
-    str.erase( std::remove( str.begin(), str.end(), ch ), str.end() ) ;
-    return str ;
+	// remove all occurrences of char ch from str
+	str.erase(std::remove(str.begin(), str.end(), ch), str.end());
+	return str;
 }
 
 std::vector<Player> read_csv(const std::string path)
 {
-  std::vector<Player> players;
-  std::string text("");
-  std::string line;
-  std::ifstream myfile (path);
-  if (myfile.is_open())
-  {
-    getline (myfile,line);
-    while ( getline (myfile,line) )
-    {
-      std::size_t found = line.find("NULL");
-      if (found != std::string::npos)
-          continue;
+	std::vector<Player> players;
+	std::string text("");
+	std::string line;
+	std::ifstream myfile(path);
+	if (myfile.is_open())
+	{
+		getline(myfile, line);
+		while (getline(myfile, line))
+		{
+			std::size_t found = line.find("NULL");
+			if (found != std::string::npos)
+				continue;
 
-      line = remove_char(line, '"');
-      std::vector<std::string> data = split(line, ',');
-      Player player;
-      player.GameID = std::stoi(data[0]);
-      player.LeagueIndex = std::stoi(data[1]);
-      player.ActualLeagueIndex = std::stoi(data[1]);
-      player.Age = std::stoi(data[2]);
-      player.HoursPerWeek = std::stoi(data[3]);
-      player.TotalHours = std::stoi(data[4]);
-      player.APM = std::stof(data[5]);
-      player.SelectByHotkeys = std::stof(data[6]);
-      player.AssignToHotkeys = std::stof(data[7]);
-      player.UniqueHotkeys = std::stof(data[8]);
-      player.MinimapAttacks = std::stof(data[9]);
-      player.MinimapRightClicks = std::stof(data[10]);
-      player.NumberOfPACs = std::stof(data[11]);
-      player.GapBetweenPACs = std::stof(data[12]);
-      player.ActionLatency = std::stof(data[13]);
-      player.ActionsInPAC = std::stof(data[14]);
-      player.TotalMapExplored = std::stof(data[15]);
-      player.WorkersMade = std::stof(data[16]);
-      player.UniqueUnitsMade = std::stof(data[17]);
-      player.ComplexUnitsMade = std::stof(data[18]);
-      player.ComplexAbilitiesUsed = std::stof(data[19]);
-      
-      players.push_back(player);
+			line = remove_char(line, '"');
+			std::vector<std::string> data = split(line, ',');
+			Player player;
+			player.GameID = std::stoi(data[0]);
+			player.LeagueIndex = std::stoi(data[1]);
+			player.ActualLeagueIndex = std::stoi(data[1]);
+			player.Age = std::stoi(data[2]);
+			player.HoursPerWeek = std::stoi(data[3]);
+			player.TotalHours = std::stoi(data[4]);
+			player.APM = std::stof(data[5]);
+			player.SelectByHotkeys = std::stof(data[6]);
+			player.AssignToHotkeys = std::stof(data[7]);
+			player.UniqueHotkeys = std::stof(data[8]);
+			player.MinimapAttacks = std::stof(data[9]);
+			player.MinimapRightClicks = std::stof(data[10]);
+			player.NumberOfPACs = std::stof(data[11]);
+			player.GapBetweenPACs = std::stof(data[12]);
+			player.ActionLatency = std::stof(data[13]);
+			player.ActionsInPAC = std::stof(data[14]);
+			player.TotalMapExplored = std::stof(data[15]);
+			player.WorkersMade = std::stof(data[16]);
+			player.UniqueUnitsMade = std::stof(data[17]);
+			player.ComplexUnitsMade = std::stof(data[18]);
+			player.ComplexAbilitiesUsed = std::stof(data[19]);
 
-      text += line +  '\n';
-    }
-    myfile.close();
-  }
+			players.push_back(player);
 
-  //else cout << "Unable to open file"; 
+			text += line + '\n';
+		}
+		myfile.close();
+	}
 
-  return players;
+	//else cout << "Unable to open file"; 
+
+	return players;
 }
 
 void split_data(std::vector<Player> *players, int nb_fold, int i,
-    std::vector<Player> *training, std::vector<Player> *test)
+	std::vector<Player> *training, std::vector<Player> *test)
 {
-  int nb_element = players->size() / nb_fold;
+	int nb_element = players->size() / nb_fold;
 
 
-  if( i > 0)
-  {
-    std::vector<Player>::const_iterator first = players->begin();
-    std::vector<Player>::const_iterator last = players->begin() + nb_element * i;
-    //*training = std::vector<Player>(first, last);
-    training->insert(training->end(), first, last);
+	if (i > 0)
+	{
+		std::vector<Player>::const_iterator first = players->begin();
+		std::vector<Player>::const_iterator last = players->begin() + nb_element * i;
+		//*training = std::vector<Player>(first, last);
+		training->insert(training->end(), first, last);
 
-  }
+	}
 
-  std::vector<Player>::const_iterator first = players->begin() + nb_element * i;
-  std::vector<Player>::const_iterator last = players->begin() + nb_element * (i + 1);
-  *test = std::vector<Player>(first, last);
+	std::vector<Player>::const_iterator first = players->begin() + nb_element * i;
+	std::vector<Player>::const_iterator last = players->begin() + nb_element * (i + 1);
+	*test = std::vector<Player>(first, last);
 
-  if ( i < nb_fold - 1)
-  {
-    std::vector<Player>::const_iterator first = players->begin() + nb_element * (i + 1);
-    std::vector<Player>::const_iterator last = players->end();
-    //*training = std::vector<Player>(first, last);
-    training->insert(training->end(), first, last);
-  }
+	if (i < nb_fold - 1)
+	{
+		std::vector<Player>::const_iterator first = players->begin() + nb_element * (i + 1);
+		std::vector<Player>::const_iterator last = players->end();
+		//*training = std::vector<Player>(first, last);
+		training->insert(training->end(), first, last);
+	}
 }
 
 void trim(std::vector<Player> *players)
@@ -158,36 +158,124 @@ void train(std::vector<Player> *players)
 
 void rate(std::vector<Player> *players)
 {
-  int score = 0;
-  for (auto& player : *players)
-    score += player.LeagueIndex == player.LeagueIndex;
-  std::cout << "the score is " << score << std::endl;
+	int score = 0;
+	for (auto& player : *players)
+		score += player.LeagueIndex == player.LeagueIndex;
+	std::cout << "the score is " << score << std::endl;
 }
 
+void normalizePlayerFieldIndexOf(std::vector<Player> *players, int playerFieldValue) {
+	float minValue = 99999.0;
+	float maxValue = 0.0;
+	float etendueValeur = 0.0;
+
+	switch (playerFieldValue) {//								IMPORTANCE : 0(low) to 10(high).
+	case 1://GameID												PAS IMPORTANT
+		break;
+	case 2://LeagueIndex										10/10
+		//Pas besoin de normalisation ecart trop petit
+		break;
+	case 3://Age												PAS IMPORTANT
+		break;
+	case 4://HoursPerWeek										PAS IMPORTANT
+		break;
+	case 5://TotalHours											PAS IMPORTANT
+		break;
+	case 6://APM													8/10
+		// Find min max etendu
+		for (auto& player : *players) {
+			if (player.APM < minValue)
+				minValue = player.APM;
+			if (player.APM > maxValue)
+				maxValue = player.APM;
+		}
+		etendueValeur = maxValue - minValue;
+
+		//Si ecart >50, on normalise avec Solution 1 normalisation Note de cours : Vi' = (Vi-Vmin)/Ek, cours 10-11 diapo 31
+		if (etendueValeur > 50) {
+			for (auto& player : *players) {
+				//float playerOldValueTest = player.APM;				//UNCOMMENT FOR TEST
+				player.APM = (((float)player.APM - (float)minValue) / etendueValeur);
+				//std::cout << "apm of a player was: " << playerOldValueTest << " and now is: " << player.APM << std::endl;		//UNCOMENT FOR TEST
+			}
+		}
+		break;
+	case 7://SelectByHotkeys										3/10
+		break;
+	case 8://AssignToHotkeys										1/10
+		break;
+	case 9://UniqueHotkeys											5/10
+		break;
+	case 10://MinimapAttacks									PAS IMPORTANT
+		break;
+	case 11://MinimapRightClicks								PAS IMPORTANT
+		break;
+	case 12://NumberOfPACs											7/10
+		break;
+	case 13://GapBetweenPACs									PAS IMPORTANT (trop petite donnee ms = precision mediocre)
+		break;
+	case 14://ActionLatency										PAS IMPORTANT (trop petite donnee ms = precision mediocre)
+		break;
+	case 15://ActionsInPAC											4/10
+		break;
+	case 16://TotalMapExplored										6/10
+		break;
+	case 17://WorkersMade											2/10
+		break;
+	case 18://UniqueUnitsMad										3/10
+		break;
+	case 19://ComplexUnitsMade									PAS IMPORTANT
+		break;
+	case 20://ComplexAbilitiesUsed									9/10
+		break;
+	}
+}
+
+/*
+	Normalize players informations :
+	Solution 1 Note de cours : Vi' = (Vi-Vmin)/Ek
+	+
+	Solution 3 Note de cours : Approx, connaissance du domaine
+*/
+void normalizePlayers(std::vector<Player> *players)
+{
+	int nb_element = players->size();
+	int minValue = 0;
+
+	//Find min value of a specific field
+	normalizePlayerFieldIndexOf(players,6);
+	for (auto& player : *players)
+		std::cout << player.APM << std::endl;
+}
+
+
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
 
 
-  std::string path("data.csv");
-  //String path = chose_file()
-  //std::string file = read_file(path );
-  std::vector<Player> players  = read_csv(path);
-  int nb_fold = 5;
-  for(int i = 0; i < nb_fold; ++i)
-  {
-    std::vector<Player> training;
-    std::vector<Player> test;
-    split_data(&players, nb_fold, i, &training, &test);
+	std::string path("data.csv");
+	//String path = chose_file()
+	//std::string file = read_file(path );
+	std::vector<Player> players = read_csv(path);
+	int nb_fold = 5;
+	for (int i = 0; i < nb_fold; ++i)
+	{
+		std::vector<Player> training;
+		std::vector<Player> test;
+		split_data(&players, nb_fold, i, &training, &test);
 
-    trim(&training);
+		normalizePlayers(&players);
 
-    train(&training);
-    //validate(&test);
+		trim(&training);
 
-    rate(&players);
-  }
+		train(&training);
+		//validate(&test);
+
+		rate(&players);
+	}
 
 
 
-  return 0;
+	return 0;
 }
